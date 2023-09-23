27c6ef8ded996ff5d917f0a58b1e81547b88605b
Your Name <you@example.com>
2023-09-23 17:46:02 +0800

    upp

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
-
-const Item_Container = document.querySelector('.Item_Container');
+const Item_Container = document.querySelector(".Item_Container");
 const navlink = document.querySelector("#navlink");
-const animated = document.querySelector('picture');
-const loader = document.querySelector('.loader')
+const animated = document.querySelector("picture");
+const loader = document.querySelector(".loader");
 
 // my Api
 const Recipe = async () => {
-const Search =  document.querySelector('input');
-const apiKey = '0+MqXToHwjmS0i7+RVhE1Q==BhuNr2wmhrzkujvH';
-const apiEndpoint = `https://api.api-ninjas.com/v1/recipe?query=${Search.value}`
+  const Search = document.querySelector("input");
+  const apiKey = "0+MqXToHwjmS0i7+RVhE1Q==BhuNr2wmhrzkujvH";
+  const apiEndpoint = `https://api.api-ninjas.com/v1/recipe?query=${Search.value}`;
 
- const data = await fetch(apiEndpoint,{
-    method:'GET',
-    headers:{'X-Api-Key': apiKey},
-    contentType: 'application/json'
- })
- const result = await  data.json();
-result.forEach(e =>{
-    const content = document.createElement('section');
+  const data = await fetch(apiEndpoint, {
+    method: "GET",
+    headers: { "X-Api-Key": apiKey },
+    contentType: "application/json",
+  });
+  const result = await data.json();
+  result.forEach((e) => {
+    const content = document.createElement("section");
     content.innerHTML = `
     <h1>${e.title}</h1>
         <p>
@@ -34,44 +33,45 @@ result.forEach(e =>{
         </details>
     
     `;
-    Item_Container.appendChild(content)
-    content.classList.add("content")
-    
-})
+    Item_Container.appendChild(content);
+    content.classList.add("content");
+  });
 
- Search.value = "";
-}
+  Search.value = "";
+};
 
-document.querySelector('form').addEventListener('submit',(e)=>{
-   e.preventDefault()
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
-})
+const btnSearch = document
+  .getElementById("btnSearch")
+  .addEventListener("click", () => {
+    loader.classList.add("active_LOADER");
+    setTimeout(() => {
+      loader.classList.remove("active_LOADER");
+      Recipe();
+      animated.classList.add("deactivated");
+    }, 4000);
+  });
 
-const btnSearch = document.getElementById('btnSearch').addEventListener('click',()=>{
-    loader.classList.add("active_LOADER")
-    setTimeout(()=>{
-        loader.classList.remove("active_LOADER")
-        Recipe()
-        animated.classList.add("deactivated")
-    },4000)
-})
-
-const burger = document.querySelector("#burger").addEventListener('click',()=>{
-navlink.classList.toggle("activeNav");
-})
-
+const burger = document
+  .querySelector("#burger")
+  .addEventListener("click", () => {
+    navlink.classList.toggle("activeNav");
+  });
 
 // FAQ JS
 
-let span = document.querySelectorAll(" h5 .span")
-let contents = document.querySelectorAll(".infos")
+let span = document.querySelectorAll(" h5 .span");
+let contents = document.querySelectorAll(".infos");
 
-span.forEach((e,index_span)=>{
-    e.addEventListener("click",()=>{
-      contents.forEach((e,index)=>{
-      if(index === index_span){
-      e.classList.toggle("active_Contents")
+span.forEach((e, index_span) => {
+  e.addEventListener("click", () => {
+    contents.forEach((e, index) => {
+      if (index === index_span) {
+        e.classList.toggle("active_Contents");
       }
-      })
-    })
-})
+    });
+  });
+});
